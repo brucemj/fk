3903d53b3c7209547630cd361c5b39ffb23281cf
mojun <mojun@konka.com>
2014-09-29 16:14:34 +0800

    root commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""
+"http://www.w3.org/TR/html4/loose.dtd">
+<head>
+    <script type="text/javascript" charset="utf-8" src="./jquery-1.7.2.min.js"></script>
+
+    <script type="text/javascript" charset="utf-8">
+        $().ready( function(){
+            $("td.data1-3").css("background-color", "#FFFFFF");
+
+        })    
+    </script>
+<style type="text/css">
+    tr,td{background-color:#A7C942; width: 30px ; height: 30px; margin: 0px; padding: 0px; border: 0px }    
+    table{width: 360px ; height: 600px ; margin: 0px; padding:0px; border: 0px; border-collapse:collapse }    
+</style>
+</head>
+
+<html>
+<body>
+    
+    <table width="300px" height="300px">
+        <?php 
+        for($i=0; $i<20; $i++){
+            echo '<tr class="row'. $i .'">';
+            for($j=0; $j<12; $j++){
+                echo '<td class="data'. $i."-".$j .'">';
+                echo '';
+                echo '</td>';
+            }
+            echo '</tr>';
+        }
+        ?>
+    </table>
+</body>
+</html>
